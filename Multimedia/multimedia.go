931a729d4460a413d209e388f1e557e0b4a12362
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-21 19:05:47 -0500

    Agregada función para mostrar valores

--- Multimedia/multimedia.go
+++ Multimedia/multimedia.go
@@ -1,19 +1,50 @@
 package Multimedia
 
+import "fmt"
+
+type Multimedia interface {
+	Show()
+}
+
+type DigitalContent struct {
+	Data []Multimedia
+}
+
 type Image struct {
-	title   string
-	format  string
-	chanels string
+	Title    string
+	Format   string
+	Channels string
 }
 
 type Audio struct {
-	title  string
-	format string
-	lenght int
+	Title  string
+	Format string
+	Lenght float64
 }
 
 type Video struct {
-	title  string
-	format string
-	frames float64
+	Title  string
+	Format string
+	Frames float64
+}
+
+func (image *Image) Show() {
+	fmt.Println("--------------IMAGE----------------------")
+	fmt.Println("Title:\t\t ", image.Title)
+	fmt.Println("Format:\t\t ", image.Format)
+	fmt.Println("Channels:\t ", image.Channels)
+}
+
+func (audio *Audio) Show() {
+	fmt.Println("---------------AUDIO----------------------")
+	fmt.Println("Title:\t ", audio.Title)
+	fmt.Println("Format:\t ", audio.Format)
+	fmt.Println("Lenght:\t ", audio.Lenght)
+}
+
+func (video *Video) Show() {
+	fmt.Println("---------------VIDEO-----------------------")
+	fmt.Println("Title:\t ", video.Title)
+	fmt.Println("Format:\t ", video.Format)
+	fmt.Println("Frames:\t ", video.Frames)
 }
--- main.go
+++ main.go
@@ -1,28 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	Multimedia "./Multimedia"
+)
+
+const (
+	IMAGE string = "image"
+	AUDIO string = "audio"
+	VIDEO string = "video"
+)
+
+func addContent(media *Multimedia.DigitalContent, _type string) {
+	var title, format, channels string
+	var lenght, frames float64
+
+	// Same info
+	fmt.Println("------------------------------------")
+	fmt.Print("Title: ")
+	fmt.Scanln(&title)
+	fmt.Print("Format: ")
+	fmt.Scanln(&format)
+
+	// Different info
+	switch _type {
+	case IMAGE:
+		fmt.Print("Channels: ")
+		fmt.Scanln(&channels)
+
+		img := Multimedia.Image{Title: title, Format: format, Channels: channels}
+		media.Data = append(media.Data, &img)
+
+		break
+	case AUDIO:
+		fmt.Print("Lenght: ")
+		fmt.Scanln(&lenght)
+
+		audio := Multimedia.Audio{Title: title, Format: format, Lenght: lenght}
+		media.Data = append(media.Data, &audio)
+
+		break
+	case VIDEO:
+		fmt.Print("Frames: ")
+		fmt.Scanln(&frames)
+
+		vid := Multimedia.Audio{Title: title, Format: format, Lenght: lenght}
+		media.Data = append(media.Data, &vid)
+
+		break
+	}
+
+}
+
+func showContent(media *Multimedia.DigitalContent) {
+	for _, c := range media.Data {
+		c.Show()
+	}
+	fmt.Print("Presione enter para continuar...")
+	fmt.Scanln()
+}
 
 func main() {
 	var opt string
+	media := Multimedia.DigitalContent{}
 
 	for opt != "0" {
-		fmt.Println("")
+		fmt.Println("\n------------------------------")
 		fmt.Println("1-. Add image")
 		fmt.Println("2-. Add audio")
 		fmt.Println("3-. Add video")
 		fmt.Println("4-. Show content")
 		fmt.Println("0-. Exit")
-		fmt.Print("\nSelect an option:")
+		fmt.Print("\nSelect an option: ")
 		fmt.Scanln(&opt)
 
 		switch opt {
 		case "1":
+			addContent(&media, IMAGE)
 			break
 		case "2":
+			addContent(&media, AUDIO)
 			break
 		case "3":
+			addContent(&media, VIDEO)
 			break
 		case "4":
+			showContent(&media)
 			break
 		}
 	}
